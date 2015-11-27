56ed44a7d6ad4bd587ac4df375e2b73578214bd3
oleh329 <creativeapp66@gmail.com>
2015-11-27 16:05:18 +0200

    Threed Commit

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+
+	public Text textField;
+
+	// Use this for initialization
+	void Start () {
+		textField.text = "IsWork";
+		textField.text = "IsWorkThree";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
